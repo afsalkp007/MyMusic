8f67173472939bfc298aa6193ef03caea7bb22a6
MohammedAfsal <afsalkp.mohammed@gmail.com>
2020-06-18 09:39:21 +0400

    Initial commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Afsal's Macbook Pro on 18/06/2020.
+//  Copyright © 2020 Afsal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
